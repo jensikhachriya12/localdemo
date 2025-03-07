f96ca909ed51f1abd3273246df65a4425acf1440
jensikhachriya <jensimukeshbhai@gmail.com>
2025-03-07 15:55:22 +0530

    second

--- Day2/2.3.py
+++ Day2/2.3.py
@@ -1,9 +1,9 @@
 def convert_second(time):
-    hour, minute,second = map(int,time.split(':'))
+    hour, minute,second = map(int, time.split(':'))
     seconds=(hour*3600)+(minute*60)+second
     return seconds
 
-time=input("enter time:")
+time=input("enter time hh:mm:ss:")
 result=convert_second(time)
 print("result:",result)
 
