684ed69335bad07d29f3d2892f556efda89e6504
jensikhachriya <jensimukeshbhai@gmail.com>
2025-03-07 15:12:29 +0530

    ball

---
+++ Day2/2.2.py
@@ -0,0 +1,8 @@
+def calculate_overs(balls):
+    overs=balls/6
+    ball= balls%6
+    return float(f"{overs}.{ball}")
+
+balls=int(input("Enter number of balls:"))
+result=calculate_overs(balls)
+print("result:",result)
